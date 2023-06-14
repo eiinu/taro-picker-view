c56c7203ee66694e17c9397ff25d92ca0775eb6a
Eiinu <eiinu@qq.com>
2023-06-14 18:24:38 +0800

    init commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <view style="height: 300px;">
+    <picker-view :value="value" @change="onChange">
+      <picker-view-column v-for="(column, index) in columns">
+        <view v-for="(item, index) in column.list" :key="index">{{ item.name }}</view>
+      </picker-view-column>
+    </picker-view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name: "Index",
+    data() {
+      return {
+        columns: [
+          {
+            list: [
+              { name: '选项一' },
+              { name: '选项二' },
+              { name: '选项三' },
+            ],
+          }
+        ],
+        value: [1],
+      }
+    },
+    methods: {
+      onChange: function(e) {
+        console.log('onChange', e.detail.value)
+        this.value = e.detail.value
+      }
+    }
+  }
+</script>
